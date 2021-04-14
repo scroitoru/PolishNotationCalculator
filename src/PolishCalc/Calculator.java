e63c6e15e1586cd2abd53a326846538c74eb5726
scroitoru <58050635+scroitoru@users.noreply.github.com>
2021-04-14 15:32:30 -0400

    basic calc completed

--- src/PolishCalc/Calculator.java
+++ src/PolishCalc/Calculator.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class PolishCalculator {
+package PolishCalc;
+
+import java.util.Stack;
+
+public class Calculator {
+
+    /**
+     * @param x
+     * makes sure it is between 0 - 9
+     */
+    public static boolean isOperand(char x) {
+        //making sure operands are between 0 and 9 (ASCII)
+        return x >= 48 && x <= 57;
+    }
+
+    /**
+     * @param expression
+     * keeps track of the operators and operands through a stack
+     * every time operator is encountered, pops 2 operands and evaluates
+     * then pushes the result back onto the stack
+     */
+    public static double evaluate(String expression) {
+        Stack<Double> stack = new Stack<>();
+
+        for (int currOp = expression.length() - 1; currOp >= 0; currOp--) {
+            if (isOperand(expression.charAt(currOp))) {
+                //convert to double - 0
+                stack.push((double) (expression.charAt(currOp) - 48));
+            } else {
+                //encountered operator, so pops two operands from stack to evaluate
+                double op1 = stack.peek();
+                stack.pop();
+                double op2 = stack.peek();
+                stack.pop();
+
+                switch (expression.charAt(currOp)) {
+                    case '+':
+                        stack.push(op1 + op2);
+                        break;
+                    case '-':
+                        stack.push(op1 - op2);
+                        break;
+                    case '*':
+                        stack.push(op1 * op2);
+                        break;
+                    case '/':
+                        stack.push(op1 / op2);
+                        break;
+                }
+            }
+        }
+        return stack.peek();
+    }
 }
+
--- src/PolishCalc/Main.java
+++ src/PolishCalc/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+package PolishCalc;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("POLISH NOTATION CALCULATOR:\n" +
+                "Prefix notation only, no spaces");
+        while (true) {
+            try {
+                System.out.println("Please enter the prefix expression you would like evaluated (NO SPACES): ");
+                String expression = sc.next();
+                System.out.println(Calculator.evaluate(expression));
+            }catch (Exception e){
+                System.out.println("invalid due to exception: " + e);
+                System.out.println("Goodbye");
+                System.exit(-1);
+            }
+        }
+
+    }
 }
